1c4a1dbb8bd25b3157e3e3ab5ae9a8be0168908b
Adam Pearson <adam.pearson94@gmail.com>
2021-08-18 22:15:10 +0100

    Initial Commit

--- CreateFile.php
+++ CreateFile.php
@@ -5,19 +5,17 @@ function buildContents($max) {
     $f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
     $string = "";
     $string .= "function isEven(number) {\n";
-    $string .= "\tif (number === 1 || number === '" . $f->format(1) . "' || number === '" . strtoupper($f->format(1)) . "' || number === '" . ucfirst($f->format(1)) . "') return false;\n";
+    $string .= "\tif (number === 1 || number === '" . $f->format(1) . "' || number === '" . strtoupper($f->format(1)) . "' || number === '" . ucfirst($f->format(1)) . "') {return false;}\n";
     for ($i = 1; $i <= $max; $i++) {
         if ($i % 2 === 0 && $i != 1) {
-            $string .= "\t\telse if (number === $i || number === '" . $f->format($i) . "' || number === '" . strtoupper($f->format($i)) . "' || number === '" . ucfirst($f->format($i)) . "' || number === '" . ucwords($f->format($i)) . "') return true;\n";
-        } elseif ($i != 1) {
-            $string .= "\t\telse if (number === $i || number === '" . $f->format($i) . "' || number === '" . strtoupper($f->format($i)) . "' || number === '" . ucfirst($f->format($i)) . "' || number === '" . ucwords($f->format($i)) . "') return false;\n";
+            $string .= "\tif (number === $i || number === '" . $f->format($i) . "' || number === '" . strtoupper($f->format($i)) . "' || number === '" . ucfirst($f->format($i)) . "' || number === '" . ucwords($f->format($i)) . "') {return true;}\n";
         }
     }
+    $string .= "\telse {return false;}\n";
     $string .= "}\n";
-    $string .= "module.exports = isEven";
     return $string;
 };
-fwrite($buildFile, buildContents(2800));
+fwrite($buildFile, buildContents(500000));
 fclose($buildFile);
 
 ?>
--- app.js
+++
@@ -1,1 +0,0 @@
-var isEven = require (["./isEven.js"]);
